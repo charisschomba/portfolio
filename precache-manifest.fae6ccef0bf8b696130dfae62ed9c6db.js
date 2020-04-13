271c52b4155d876b28fee732f1bfabf98b177780
chariss <chariss.karunjira@andela.com>
2020-04-13 16:43:37 +0300

    Updates

---
+++ precache-manifest.fae6ccef0bf8b696130dfae62ed9c6db.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e542e16c24669075d1822375069fc33",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0c62293a317bafec47db",
+    "url": "/portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3a7c1af5d9f7acf2e3bd",
+    "url": "/portfolio/static/css/main.39015d67.chunk.css"
+  },
+  {
+    "revision": "0c62293a317bafec47db",
+    "url": "/portfolio/static/js/2.858d3e2d.chunk.js"
+  },
+  {
+    "revision": "6ef718eeb39c03c0fcdab1a60b0c6b00",
+    "url": "/portfolio/static/js/2.858d3e2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7c1af5d9f7acf2e3bd",
+    "url": "/portfolio/static/js/main.19f73108.chunk.js"
+  },
+  {
+    "revision": "3ca375981f9b833547c1",
+    "url": "/portfolio/static/js/runtime-main.0ba2c954.js"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/portfolio/static/media/Poppins-Medium.ba95810b.ttf"
+  },
+  {
+    "revision": "bf5b03a8df8999612c067a7e3581d751",
+    "url": "/portfolio/static/media/home.bf5b03a8.jpg"
+  },
+  {
+    "revision": "d9f410a1c1c2555a0f16267b7c7f8ed8",
+    "url": "/portfolio/static/media/me.d9f410a1.jpg"
+  }
+]);
